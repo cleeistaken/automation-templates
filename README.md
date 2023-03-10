da435b0583e2631e1874b005573a41ee48a198e1
Charles Lee <charlesl@isvlab.vmware.com>
2023-03-10 13:25:15 -0600

    added template and file download

---
+++ README.md
@@ -0,0 +1,9 @@
+# automation-templates
+This is a simple ansible script to setup a local nginx web server to server templates and files for other automation projects.
+
+## Requirements
+This project was tested on Ubuntu 22.04. 
+
+## Folder
+* **data** will contain the temmplates and files
+* **results** will contain the published tests results
--- run.sh
+++ run.sh
@@ -1,5 +1,35 @@
 #! /bin/bash
 
+echo "Installing vim"
+sudo apt install -y vim
+
+echo "Install python"
+sudo apt install -y python3
+
+echo "Upgrade python pip"
+python3 -m pip install pip --upgrade
+
+echo "Upgrade python setuptools"
+python3 -m pip install setuptools --upgrade
+
+echo "Install pipenv"
+python3 -m pip install pipenv --upgrade
+
+if [ ! ~/.ssh/id_rsa ]
+then
+ echo "Generate new id_rsa"
+ ssh-keygen -q -t rsa -N '' <<< $'\ny' > /dev/null 2>&1
+else
+  echo "id_rsa already exists"
+fi
+
+echo "Install terraform"
+sudo snap install terraform --classic
+
+echo "Install docker"
+sudo apt install -y docker.io
+sudo usermod -aG docker `whoami`
+
 # Nasty bug
 # Ref. https://github.com/pypa/pipenv/issues/5075
 echo "Exporting SETUPTOOLS_USE_DISTUTILS"
@@ -12,3 +42,5 @@ echo "Running ansible"
 pushd ansible > /dev/null
 pipenv run ansible-playbook main.yml --ask-become-pass
 popd > /dev/null
+
+echo "Done"
